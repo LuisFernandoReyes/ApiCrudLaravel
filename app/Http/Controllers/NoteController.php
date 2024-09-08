e988a3c8a2e14e27104100f5abcd33bc8fdcd722
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-09-07 18:57:09 -0600

    Makin the first api and I watched until 49:55 at cap 6 í

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -5,42 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Note;
 use App\Http\Requests\NoteRequest;
+use Illuminate\Http\JsonResponse;
 
 class NoteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    public function index():JsonResponse
     {
         $notes = Note::all();
         return response()->json($notes, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function store(NoteRequest $request)
+    public function store(NoteRequest $request):JsonResponse
     {
-        Note::create($request->all());
+        $note= Note::create($request->all());
         return response()->json([
-            'success' => true
+            'success' => true,
+            'data' => $note
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(string $id):JsonResponse
     {
         $note = Note::find($id);
         return response()->json($note, 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(NoteRequest $request, $id)
+    public function update(NoteRequest $request, $id):JsonResponse
     {
         $note= Note::find($id);
         $note->title = $request->title;
@@ -48,11 +39,12 @@ class NoteController extends Controller
         $note->save();
 
         return response()->json([
-            'success' => true
+            'success' => true,
+            'data' => $note
         ], 200);
         }
 
-    public function destroy(string $id)
+    public function destroy(string $id):JsonResponse
     {
         Note::find($id)->delete();
         return response()->json([
--- app/Http/Requests/NoteRequest.php
+++ app/Http/Requests/NoteRequest.php
@@ -12,7 +12,7 @@ class NoteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return TrustProxies;
+        return true;
     }
 
     /**
